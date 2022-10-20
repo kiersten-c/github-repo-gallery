df87702f61bc26b3e968902ada7bc2a8185505eb
Kiersten C <112491400+kiersten-c@users.noreply.github.com>
2022-10-20 16:23:47 -0400

    Added API to fetch repo data and display

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //div where profile info will appear
 const profileInfo = document.querySelector(".overview");
+//ul where repo list appears
+const displayRepos = document.querySelector(".repo-list");
 const username = "kiersten-c"
 
 const gitHubUser = async function(){
@@ -8,6 +10,8 @@ const gitHubUser = async function(){
     // console.log(data);
     displayUserInfo(data);
 };
+
+gitHubUser();
 
 const displayUserInfo = function(data){
     const userInfo = document.createElement("div");
@@ -23,6 +27,21 @@ const displayUserInfo = function(data){
           <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>`
     profileInfo.append(userInfo);
+    getRepoData();
 };
 
-gitHubUser();
+const getRepoData = async function(){
+    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=created&per_page=100`);
+    const data = await response.json();
+    // console.log(data);
+    displayRepoInfo(data);
+};
+
+const displayRepoInfo = function(repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML=`<h3>${repo.name}</h3>`
+        displayRepos.append(repoItem);
+    }
+};
